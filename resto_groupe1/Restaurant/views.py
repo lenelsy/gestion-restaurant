777fc34f6ce070d08f5926eeda4441e5789eba78
lenelsy <kouamnelson23@gmail.com>
2026-04-26 12:12:45 +0100

    Delete resto_groupe1 directory

--- resto_groupe1/Restaurant/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from .models import Produit
-
-def catalogue_produits(request):
-    # Récupère tous les plats (Poulet DG, Ndolé, etc.) de ta base SQL
-    plats = Produit.objects.all()
-    return render(request, 'Restaurant/catalogue.html', {'plats': plats})
