a392cd8fe86b6b2bce5ca7804061f19e8d6d068d
lenelsy <kouamnelson23@gmail.com>
2026-04-26 12:35:35 +0100

    1er et 2e modules

--- resto_groupe1/Restaurant/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Table, Employe, Produit, Commande, LigneCommande
-
-admin.site.register(Produit)
-admin.site.register(Table)
-admin.site.register(Employe)
-admin.site.register(Commande)
-admin.site.register(LigneCommande)
--- resto_groupe1/Restaurant/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('catalogue/', views.catalogue_produits, name='catalogue'),
-]
